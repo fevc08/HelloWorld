cb098e54a23ef9e7b41c8cac69fa2f611df2e6b1
Fidel Vera <fevc1988@gmail.com>
2024-02-19 12:25:03 -0300

    Variables

---
+++ src/HolaMundo.java
@@ -0,0 +1,10 @@
+//Mi clase en Java
+public class HolaMundo {
+    public static void main(String args[]){
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+    }
+}
